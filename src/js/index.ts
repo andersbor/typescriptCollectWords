1a798651d13973f3235ca804ed085afd1424c6ee
andersbor <anbo@easj.dk>
2018-11-19 08:54:10 +0100

    save, show, clear

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,40 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let words: string[] = []; // empty array
+
+let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById("wordInput");
+
+let saveButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("saveButton");
+saveButton.addEventListener("click", saveWord);
+
+let showButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("showButton");
+showButton.addEventListener("click", showWords);
+
+let clearButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("clearButton");
+clearButton.addEventListener("click", clearWords);
+
+let outputElement: HTMLElement = <HTMLElement>document.getElementById("output");
+
+console.log("Ready");
+
+function showWords(): void {
+    if (words.length > 0) {
+        outputElement.innerHTML = words.toString();
+        outputElement.style.fontStyle = "normal";
+    } else {
+        outputElement.innerHTML = "empty";
+        outputElement.style.fontStyle = "italic";
+    }
+    console.log("Show");
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function clearWords(): void {
+    words = [];
+    outputElement.innerHTML = "";
+    console.log("Cleared" + words);
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function saveWord(): void {
+    let word: string = inputElement.value;
+    words.push(word);
+    console.log("Save word");
+    return;
+}
